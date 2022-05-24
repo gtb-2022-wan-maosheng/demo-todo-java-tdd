fd42fb98d8b399cdbbf5855480b78ef98f2689e2
maoshengw <wanmaoshenggl@gmail.com>
2022-05-24 11:50:49 +0800

    delete default test and source code by Gradle

--- app/src/main/java/com/wms/tw/gtb/todo/App.java
+++ app/src/main/java/com/wms/tw/gtb/todo/App.java
@@ -1,12 +1,7 @@
-
 package com.wms.tw.gtb.todo;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
     }
 }
--- app/src/test/java/com/wms/tw/gtb/todo/AppTest.java
+++ app/src/test/java/com/wms/tw/gtb/todo/AppTest.java
@@ -1,14 +1,4 @@
-
 package com.wms.tw.gtb.todo;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 class AppTest {
-    @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
 }
