89dad81d281bdae3c42f21bdc98621e8d204939c
maoshengw <wanmaoshenggl@gmail.com>
2022-05-25 14:08:31 +0800

    feat: add test for task name with mutiple whitespaces

--- app/src/test/java/com/wms/tw/gtb/todo/TaskFactoryTest.java
+++ app/src/test/java/com/wms/tw/gtb/todo/TaskFactoryTest.java
@@ -1,5 +1,6 @@
 package com.wms.tw.gtb.todo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -9,5 +10,11 @@ class TaskFactoryTest {
     void should_parse_completed_property_for_task() {
         final var isCompleted = TaskFactory.createTask(1, "+ foobar").isCompleted();
         assertFalse(isCompleted);
+    }
+
+    @Test
+    void should_support_name_with_multiple_whitespaces() {
+        final var task = TaskFactory.createTask(1, "+     foo    bar  ");
+        Assertions.assertEquals("    foo    bar  ", task.getName());
     }
 }
