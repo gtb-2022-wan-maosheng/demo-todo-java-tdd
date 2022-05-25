498c405488696638c4d659751e8b708ec80b66a0
maoshengw <wanmaoshenggl@gmail.com>
2022-05-25 10:34:38 +0800

    wip

--- app/src/main/java/com/wms/tw/gtb/todo/App.java
+++ app/src/main/java/com/wms/tw/gtb/todo/App.java
@@ -12,13 +12,26 @@ public class App {
     }
 
     public List<String> run() {
-        final List<String> lines = readTaskLines();
+        final List<Task> tasks = loadTasks();
         final var result = new ArrayList<String>();
         result.add("# To be done");
-        for (int i = 0; i < lines.size(); i++) {
-            result.add(String.format("%d %s", i + 1, lines.get(i)));
+        for(var task : tasks){
+            result.add(task.format());
         }
         return result;
+    }
+
+    private List<Task> loadTasks() {
+        final List<String> lines = readTaskLines();
+        final List<Task> tasks = new ArrayList<>();
+        for (int i = 0; i < lines.size(); i++) {
+            final var id = i + 1;
+            final var line = lines.get(i);
+            final var name = line;
+            final var task = new Task(id, name);
+            tasks.add(task);
+        }
+        return tasks;
     }
 
     private List<String> readTaskLines() {
---
+++ app/src/main/java/com/wms/tw/gtb/todo/Task.java
@@ -0,0 +1,31 @@
+package com.wms.tw.gtb.todo;
+
+public class Task {
+    private int id;
+    private String name;
+
+    public Task(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    String format() {
+        return String.format("%d %s", getId(), getName());
+    }
+}
