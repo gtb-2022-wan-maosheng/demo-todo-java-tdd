a627c2be8fdff36643917b776ff65e945e37ab37
maoshengw <wanmaoshenggl@gmail.com>
2022-05-30 10:15:35 +0800

    feat: add task with single word as name

--- app/src/main/java/com/wms/tw/gtb/todo/App.java
+++ app/src/main/java/com/wms/tw/gtb/todo/App.java
@@ -1,5 +1,9 @@
 package com.wms.tw.gtb.todo;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class App {
@@ -10,7 +14,17 @@ public class App {
         new App().run().forEach(System.out::println);
     }
 
-    public List<String> run() {
+    public List<String> run(String... args) {
+        if (args.length > 0 && args[0].equals("add")){
+            try (var bufferedWriter = Files.newBufferedWriter(Constants.TASK_FILE_PATH, StandardOpenOption.APPEND)) {
+                final var taskName = args[1];
+                bufferedWriter.write("+ " + taskName);
+                bufferedWriter.newLine();
+            } catch (IOException e) {
+                throw new TodoCannotReadFileException();
+            }
+            return List.of();
+        }
         return listCommand.run();
     }
 
--- app/src/test/java/com/wms/tw/gtb/todo/AppTest.java
+++ app/src/test/java/com/wms/tw/gtb/todo/AppTest.java
@@ -20,4 +20,20 @@ class AppTest {
                 result);
 
     }
+
+    @Test
+    void shoule_add_task_with_single_world_as_name() {
+        new App().run("add", "foobar");
+        final var result = new App().run();
+        Assertions.assertEquals(List.of(
+                "# To be done",
+                "1 Task 01",
+                "2 Task 02",
+                "5 foobar",
+                "# Completed",
+                "3 Task 03",
+                "4 Task 04"),
+                result);
+
+    }
 }
